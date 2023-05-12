2a62280802b4d701bf602572dfe56d7926155daf
Moieen Abbas <moieen.abbas@ascertia.com>
2023-05-13 03:54:42 +0500

    initial commit

---
+++ ProjectExe/Com/Ascertia/Test/API/Exec/CallAPIFromExe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Ascertia.Test.API.Exec
+{
+    public class CallAPIFromExe
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calling Hello Service from EXE");
+            new API().sayHelloFromNet("called from exe");
+        }
+    }
+}
---
+++ ProjectLib/Com/Ascertia/Test/API/API.cs
@@ -0,0 +1,22 @@
+﻿using com.ascertia.test.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Ascertia.Test.API
+{
+    public class API
+    {
+       public void sayHelloFromNet(String name)
+        {
+            com.ascertia.test.service.HelloServiceLoader.loadMyHelloService().sayHello(name);
+        }
+
+        public static void Main(String[] args)
+        {
+            new API().sayHelloFromNet("called from projectLib");
+        }
+    }
+}
